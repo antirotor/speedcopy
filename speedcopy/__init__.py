1a009a279a70f005f01f6fed5d30aa90a2baf54e
Ondrej Samohel <ondrej.samohel@ynput.io>
2026-04-24 23:34:07 +0200

    :recycle: re-export `SPEEDCOPY_DEBUG`

--- speedcopy/__init__.py
+++ speedcopy/__init__.py
@@ -1,8 +1,14 @@
 """Speedcopy."""
-from .copyfile import copyfile, patch_copyfile, unpatch_copyfile
+from .copyfile import (
+    SPEEDCOPY_DEBUG,
+    copyfile,
+    patch_copyfile,
+    unpatch_copyfile,
+)
 from .version import __version__
 
 __all__ = [
+    "SPEEDCOPY_DEBUG",
     "__version__",
     "copyfile",
     "patch_copyfile",
